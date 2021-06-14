2ef7cd35f469a820eb7b9ccda23864ae7c1b6a59
Celso Campos <ballistictechnician@outlook.com>
2021-06-14 19:06:50 -0300

    Atualizando lições aula 4

--- src/Tabuada.java
+++ src/Tabuada.java
@@ -6,7 +6,7 @@ public class Tabuada {
 	int i=0;
 	int num;
 	int result;
-	System.out.println("Entre com um numero para tabuada do 1 ao 10");
+	System.out.println("Entre com um numero para receber a tabuada do 1 ao 10");
 	num = leitor.nextInt();
 	while(i<10) {
 		i=i+1;
